6a170fa32cd89a5a3e5ac19c1d313865309bc720
TahaS-Coding <143848726+TahaS-Coding@users.noreply.github.com>
2024-05-08 21:43:28 -0700

    Working on resume

--- home.js
+++ home.js
@@ -1,18 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     let heading = document.getElementById("homeHeading");
+    headingAnimation();
     setInterval(headingAnimation, 4000);
     function headingAnimation(){
-        switch(heading.innerText){
-            case "High School Student":
-                heading.innerText = "Developer";
-                break;
-            case "Developer":
-                heading.innerText = "Your Future Employee";
-                break;
-            case "Your Future Employee":
-                heading.innerText = "High School Student";
-                break;
-        }
+        heading.style.opacity = 0;
+        // wait for heading to fade before switching title with setTimeout
+        setTimeout(() => {
+            switch(heading.innerText){
+                case "Title":
+                    heading.innerText = "High School Student";
+                    break;
+                case "High School Student":
+                    heading.innerText = "Developer";
+                    break;
+                case "Developer":
+                    heading.innerText = "Your Future Employee";
+                    break;
+                case "Your Future Employee":
+                    heading.innerText = "High School Student";
+                    break;
+            }
+            heading.style.opacity = 1;
+        }, 500);
     }
 })
