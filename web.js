5f832b476dff4057cae2afb93d668d2312c1499b
TahaS-Coding <143848726+TahaS-Coding@users.noreply.github.com>
2024-05-13 15:48:55 -0700

    Finished desktop styles

--- web.js
+++ web.js
@@ -5,6 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
             case jurassicImage:
                 transition = jurassicTransition;
                 break;
+            case calculatorImage:
+                transition = calculatorTransition;
+                break;
+            case battleshipImage:
+                transition = battleshipTransition;
+                break;
         }
         transition.style.opacity = 1;
         setTimeout(() => {
@@ -13,6 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 150);
     }
 
+    // jurassic carousel
     let jurassicLeft = document.getElementById("jurassicLeft");
     let jurassicRight = document.getElementById("jurassicRight");
     let jurassicImage = document.getElementById("jurassicImage") ;
@@ -62,6 +69,66 @@ document.addEventListener("DOMContentLoaded", function () {
             case "images/jurassic-six.png":
                 jurassicRight.style.backgroundColor = "#437aa1";
                 timeoutFunction(jurassicImage, "images/jurassic-seven.png");
+                break;
+        }
+    });
+
+    //calculator carousel
+    let calculatorLeft = document.getElementById("calculatorLeft");
+    let calculatorRight = document.getElementById("calculatorRight");
+    let calculatorImage = document.getElementById("calculatorImage") ;
+    let calculatorTransition = document.getElementById("calculatorTransition");
+    calculatorLeft.addEventListener("click", function(){
+        switch (calculatorImage.getAttribute("src")){
+            case "images/calculator-two.png":
+                calculatorLeft.style.backgroundColor = "#437aa1";
+                timeoutFunction(calculatorImage, "images/calculator-one.png");
+                break;
+            case "images/calculator-three.png":
+                calculatorRight.style.backgroundColor = "#5091BF";
+                timeoutFunction(calculatorImage, "images/calculator-two.png");
+                break;
+        }
+    });
+    calculatorRight.addEventListener("click", function(){
+        switch (calculatorImage.getAttribute("src")){
+            case "images/calculator-one.png":
+                calculatorLeft.style.backgroundColor = "#5091BF";
+                timeoutFunction(calculatorImage, "images/calculator-two.png");
+                break;
+            case "images/calculator-two.png":
+                calculatorRight.style.backgroundColor = "#437aa1";
+                timeoutFunction(calculatorImage, "images/calculator-three.png");
+                break;
+        }
+    });
+
+    //battleship carousel
+    let battleshipLeft = document.getElementById("battleshipLeft");
+    let battleshipRight = document.getElementById("battleshipRight");
+    let battleshipImage = document.getElementById("battleshipImage") ;
+    let battleshipTransition = document.getElementById("battleshipTransition");
+    battleshipLeft.addEventListener("click", function(){
+        switch (battleshipImage.getAttribute("src")){
+            case "images/battleship-two.png":
+                battleshipLeft.style.backgroundColor = "#437aa1";
+                timeoutFunction(battleshipImage, "images/battleship-one.png");
+                break;
+            case "images/battleship-three.png":
+                battleshipRight.style.backgroundColor = "#5091BF";
+                timeoutFunction(battleshipImage, "images/battleship-two.png");
+                break;
+        }
+    });
+    battleshipRight.addEventListener("click", function(){
+        switch (battleshipImage.getAttribute("src")){
+            case "images/battleship-one.png":
+                battleshipLeft.style.backgroundColor = "#5091BF";
+                timeoutFunction(battleshipImage, "images/battleship-two.png");
+                break;
+            case "images/battleship-two.png":
+                battleshipRight.style.backgroundColor = "#437aa1";
+                timeoutFunction(battleshipImage, "images/battleship-three.png");
                 break;
         }
     });
