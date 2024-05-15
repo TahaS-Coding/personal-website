4f74298d374d71a6c1f7b58d027e61a26a17494e
TahaS-Coding <143848726+TahaS-Coding@users.noreply.github.com>
2024-05-15 13:13:34 -0700

    Combined javascript files homepage

--- home.js
+++ home.js
@@ -1,5 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+    // menu.js code
+    let menuButton = document.getElementById("menuButton");
+    let dropDown = document.querySelector(".site-nav");
+    menuButton.addEventListener("click", function(){
+        if(menuButton.getAttribute("src") == "images/menu.svg"){
+            dropDown.style.width = "100vw";
+            menuButton.setAttribute("src", "images/close.svg");
+            for(anchor of document.querySelectorAll(".site-nav a")){
+                anchor.style.opacity = 1;
+            }
+            document.body.style.overflow = "hidden";
+        }
+        else{
+            dropDown.style.width = "0";
+            menuButton.setAttribute("src", "images/menu.svg");
+            for(anchor of document.querySelectorAll(".site-nav a")){
+                anchor.style.opacity = 0;
+            }
+            document.body.style.overflow = "";
+        }
+    });
+    // heading animation
     let heading = document.getElementById("homeHeading");
     headingAnimation();
     setInterval(headingAnimation, 4000);
